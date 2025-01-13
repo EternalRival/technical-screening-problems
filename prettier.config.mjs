dc5875807250ea48eea0291bfc489d8bec156eee
EternalRival <eternalrival8@gmail.com>
2025-01-13 22:34:07 +0300

    init: create repo with first task

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+import erPrettierConfig from "@eternalrival/prettier-config";
+
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  ...erPrettierConfig,
+  /* your rules */
+};
+
+export default config;
